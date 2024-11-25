8f87b8cd264b867f55b6440217b6667912f4ddd6
Borhan Uddin <68281712+wapborhan@users.noreply.github.com>
2024-11-25 22:27:38 +0600

    Create Documention Site using nextra by NextJS

---
+++ pages/_meta.js
@@ -0,0 +1,13 @@
+export default {
+  index: "",
+  about: {
+    title: "About",
+    type: "page",
+  },
+  contact: {
+    title: "Contact ↗",
+    type: "page",
+    href: "https://www.wapborhan.com/",
+    newWindow: true,
+  },
+};
---
+++ theme.config.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { DocsThemeConfig } from 'nextra-theme-docs'
+
+const config: DocsThemeConfig = {
+  logo: <span>টাইপস্ক্রিপ্ট</span>,
+  project: {
+    link: 'https://github.com/wapborhan/learn-typescript',
+  },
+  chat: {
+    link: 'https://discord.com',
+  },
+  docsRepositoryBase: 'https://github.com/wapborhan/learn-typescript',
+  footer: {
+    text: 'Nextra Docs Template',
+  }, 
+  
+}
+
+export default config
